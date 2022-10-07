f5d5f5ca0128b06156eb4cf9120ec974b95018f8
Ricky Canepa <rickycanepa.rc@gmail.com>
2022-10-07 10:40:53 -0500

    functions parameters

---
+++ main.js
@@ -0,0 +1,12 @@
+const quarterValue = (num) => {
+    let quarter = num / 4
+    return quarter
+
+}
+
+// Invoke the function and store the return value
+
+
+// Log the return value
+let quarterNum = quarterValue(8);
+console.log(quarterNum);
